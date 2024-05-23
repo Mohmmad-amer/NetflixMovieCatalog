678556eac425d1b7150f350d34cbdbc06e47cbff
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-23 16:29:45 +0300

    deploy app.5

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,7 @@ pid=$(sudo lsof -i -P -n | grep 8080 |awk 'NR==1 {print $2}')
 if [[ -z $pid ]]; then
     echo "killng process: $pid"
     echo $pid
-    echo "$pid are"
-    echo "$pid are"
-    $(kill -9 "$pid")
+    kill -9 $pid
 else
     echo "No process found using port 8080"
 fi
