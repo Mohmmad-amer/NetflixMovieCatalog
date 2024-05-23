e5ff6c746591141a638de1eee4df3e7eb008af0d
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-23 15:28:36 +0300

    deploy app.5

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UIkfvbkfdsnkln ;-)"
+    return "Hi! This app is an API, there is no UI just lame text ;-)"
 
 
 @app.route('/discover')
