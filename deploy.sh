1bdac0ecc6e140f3ca335175550b1d5d7fd15673
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-23 16:42:22 +0300

    deploy app.5

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,4 @@ cd /home/ubuntu/app
 python3 -m venv myenv
 source myenv/bin/activate
 pip install -r /home/ubuntu/app/requirements.txt
-python3 /home/ubuntu/app/app.py & disown
-exit
+python3 /home/ubuntu/app/app.py &
