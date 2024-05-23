f909f75c59ff993211f9d2454caf81fddfddf888
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-23 14:02:44 +0300

    deploy app.2

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,12 @@
 
 # TODO your deploy script implementation...
 
-/home/ubuntu/app
-
 pid=sudo lsof -i -P -n | grep 8080 |awk 'NR==1 {print $2}'
 
 if [ -n "$pid" ]; then
     echo "killng process: $pid"
     kill -9 $pid
 else
-    echo "No process found using port 8080
+    echo "No process found using port 8080"
 fi
-
-
 nohup python3 /home/ubuntu/app/NetflixMovieCatalog/app.py &
