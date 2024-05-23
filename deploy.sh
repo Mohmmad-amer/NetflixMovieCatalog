58e292eafda5cd1b3d1362008809b88083178edb
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-23 13:30:23 +0300

    deploy app

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # TODO your deploy script implementation...
+
+/home/ubuntu/app
+
+pid=sudo lsof -i -P -n | grep 8080 |awk 'NR==1 {print $2}'
+
+if [ -n "$pid" ]; then
+    echo "killng process: $pid"
+    kill -9 $pid
+else
+    echo "No process found using port 8080
+fi
+
+nohup python3 /home/ubuntu/app/app.py &
